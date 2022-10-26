d73e7909672b091a09c27ca2afbba320a0fc3626
amal ziad <git config --global user.email>
2022-10-26 21:04:35 +0300

    H2 data basse

--- src/main/java/com/example/bank_account_service/entities/BankAccount.java
+++ src/main/java/com/example/bank_account_service/entities/BankAccount.java
@@ -1,10 +1,18 @@
 package com.example.bank_account_service.entities;
 
 import com.example.bank_account_service.enums.AccountType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.util.Date;
-
+@Entity
+@Data@AllArgsConstructor@NoArgsConstructor@Builder
 public class BankAccount {
+    @Id
     private String id;
     private Date createdAt;
     private double balance;
